9c01b0b4c913211ee071bc29a1a301c76e25159c
Yash Raj <yashraj@Yashs-MacBook-Air.local>
2022-06-01 07:37:22 +0530

    zoom in zoom out controls in map tab

--- lib/codingapp/Tabs/Map_Tab.dart
+++ lib/codingapp/Tabs/Map_Tab.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
@@ -23,6 +24,7 @@ class _MyMapState extends State<MyMap> {
   final Set<Marker> _markers = {};
   GoogleMapController? mapController;
   MapType _currentMapType = MapType.satellite;
+  double flag = 0; // flag for zoom-in and zoom-out
 
   void _onMapTypeButtonPressed() {
     setState(() {
@@ -32,9 +34,11 @@ class _MyMapState extends State<MyMap> {
     });
   }
 
-  void _onAddMarkerButtonPressed() {
+  void _ongpsfixedButtonPressed() {
     setState(() {
       _fixposition();
+      flag = 0;
+
       LatLng newlatlang = _center;
       mapController?.animateCamera(
         CameraUpdate.newCameraPosition(
@@ -89,6 +93,36 @@ class _MyMapState extends State<MyMap> {
     }
   }
 
+  zoominout(double zoomflag) async {
+    dynamic credencials = await _getCredentials();
+
+    SSHClient client = SSHClient(
+      host: '${credencials['ip']}',
+      port: int.parse('${credencials['port']}'),
+      username: '${credencials['username']}',
+      passwordOrKey: '${credencials['pass']}',
+    );
+    double temp = (100569.665945696469 + zoomflag * 60000);
+    if (temp < 0) temp = 100;
+    String temp2 = temp.toString();
+    log(temp2);
+    LookAt flyto = LookAt(
+      -17.8984565,
+      28.6599744,
+      '$temp2',
+      '0',
+      '0',
+    );
+    try {
+      await client.connect();
+      await client.execute(
+          'echo "flytoview=${flyto.generateLinearString()}" > /tmp/query.txt');
+    } catch (e) {
+      print('Could not connect to host LG');
+      return Future.error(e);
+    }
+  }
+
   void _onCameraMove(CameraPosition position) {}
 
   @override
@@ -111,7 +145,10 @@ class _MyMapState extends State<MyMap> {
             target: _center,
             zoom: 10.8,
           ),
+          zoomControlsEnabled: false,
           zoomGesturesEnabled: true,
+          scrollGesturesEnabled: true,
+          compassEnabled: true,
           mapType: _currentMapType,
           markers: _markers,
           onCameraMove: _onCameraMove,
@@ -136,7 +173,7 @@ class _MyMapState extends State<MyMap> {
                 SizedBox(height: 16.0),
                 FloatingActionButton(
                   heroTag: "btn2",
-                  onPressed: _onAddMarkerButtonPressed,
+                  onPressed: _ongpsfixedButtonPressed,
                   materialTapTargetSize: MaterialTapTargetSize.padded,
                   backgroundColor: Color.fromARGB(255, 75, 127, 82),
                   child: const Icon(
@@ -149,6 +186,71 @@ class _MyMapState extends State<MyMap> {
             ),
           ),
         ),
+        Positioned(
+          bottom: 70,
+          left: 20,
+          child: Card(
+            elevation: 0,
+            child: Container(
+              color: Color.fromARGB(255, 78, 78, 78),
+              width: 59.5,
+              height: 124.25,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 6),
+                  IconButton(
+                      icon: Icon(
+                        Icons.add,
+                        color: Color.fromARGB(255, 204, 204, 204),
+                        size: 32,
+                      ),
+                      onPressed: () async {
+                        var currentZoomLevel =
+                            await mapController?.getZoomLevel();
+
+                        currentZoomLevel = (currentZoomLevel! + 1);
+                        flag -= 1;
+                        zoominout(flag);
+                        mapController?.animateCamera(
+                          CameraUpdate.newCameraPosition(
+                            CameraPosition(
+                              target: _center,
+                              zoom: currentZoomLevel,
+                            ),
+                          ),
+                        );
+                      }),
+                  SizedBox(height: 12.5),
+                  IconButton(
+                      icon: Icon(
+                        Icons.remove,
+                        size: 32,
+                        color: Color.fromARGB(255, 204, 204, 204),
+                      ),
+                      onPressed: () async {
+                        var currentZoomLevel =
+                            await mapController?.getZoomLevel();
+                        currentZoomLevel = currentZoomLevel! - 1;
+                        flag += 1;
+                        zoominout(flag);
+                        if (currentZoomLevel < 0) {
+                          currentZoomLevel = 0;
+                          flag = 0;
+                        }
+                        mapController?.animateCamera(
+                          CameraUpdate.newCameraPosition(
+                            CameraPosition(
+                              target: _center,
+                              zoom: currentZoomLevel,
+                            ),
+                          ),
+                        );
+                      }),
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     );
   }
